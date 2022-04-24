ee295978b4c0e79eda7d913f2b51226da7d891fe
Firuz43 <firuz1199@mail.ru>
2022-04-24 15:33:41 +0400

    variables

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -2,8 +2,8 @@ package com.example.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        int age = 30;
-        age = 35;
-        System.out.println(age);
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(herAge);
     }
 }
