b8ea1d1afbbea2f04856caa34057df1608dee30c
Firuz43 <firuz1199@mail.ru>
2022-04-24 16:24:25 +0400

    ff

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -2,8 +2,13 @@ package com.example.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
+//        long viewsCount = 3_123_456_789L;
+//        float price = 10.99F;
+//        char letter = 'A';
+//        boolean isEligible = false;
+        byte age = 30;
+        Date now = new Date();
+
         System.out.println(viewsCount);
     }
 }
