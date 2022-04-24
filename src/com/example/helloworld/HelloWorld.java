ac7cab94893bd58fb69568dda19bdd5ea8a9ee8e
Firuz43 <firuz1199@mail.ru>
2022-04-25 03:37:37 +0400

    constants

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,24 +1,13 @@
 package com.example.helloworld;
 
 import java.awt.*;
+import java.util.Arrays;
 //import java.util.Date;
 
 public class HelloWorld {
     public static void main(String[] args) {
-//        long viewsCount = 3_123_456_789L;
-//        float price = 10.99F;
-//        char letter = 'A';
-//        boolean isEligible = false;
-//        byte age = 30;
-//        Date now = new Date();
-//        System.out.println(now);
-//        Point point1 = new Point(1, 1);
-//        Point point2 = point1;
-//        point1.x = 2;
-//        System.out.println(point2);
-//        String message = ("Hello World" + "!!");
-//        System.out.println(message.replace("!", "*"));
-        String message = "Hello \"Firuz\"";
-        System.out.println(message);
+        float pi = 3.14F;
+        pi = 1;
+        System.out.println(pi);
     }
 }
